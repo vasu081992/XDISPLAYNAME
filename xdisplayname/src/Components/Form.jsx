2e405c113fd20d8fa012db5cde3c2a6ebb13fd32
vasu081992 <vasudevan29.92@gmail.com>
2024-05-11 11:22:47 +0530

    form handling

---
+++ xdisplayname/src/Components/Form.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styles from "./Form.module.css"
+import { useState } from "react";
+
+
+
+const Form = ()=>{
+
+
+    const [inputvalue1,setinputvalue1] = useState('');
+
+    const [inputvalue2,setinputvalue2] = useState('');
+
+    const [submit,setsubmit] = useState(false)
+
+const formSubmit =(e)=>{
+    e.preventDefault(); 
+    setsubmit(true)
+}
+
+
+
+const handleChange1 = (e)=>{
+
+    setinputvalue1(e.target.value)
+
+ }
+
+ const handleChange2 = (e)=>{
+
+
+    setinputvalue2(e.target.value)
+
+ }
+
+//  console.log("state1",inputvalue1)
+//  console.log("state2",inputvalue2)
+
+
+    return (
+<form onSubmit={formSubmit}>
+
+<h1> Full Name Display</h1>
+
+
+
+<div className={styles.wrapper1}> 
+<p>First Name :  </p>
+<input placeholder="Enter first name..."  value={inputvalue1} onChange = {handleChange1} required/>
+</div>
+<div className={styles.wrapper1}> 
+<p>Last Name :  </p>
+<input placeholder="Enter last name..."  value={inputvalue2} onChange = {handleChange2} required/>
+</div>
+
+<button type="submit" >Submit</button>
+
+{ submit? (
+<p>Full Name: {inputvalue1}  {inputvalue2} </p>
+) : ""
+}
+
+</form>
+
+
+    )
+}
+
+export default Form;
